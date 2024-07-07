bf06fa3919d50556fd23a09bf31d880923506364
plesen666 <diada.var@mail.ru>
2024-07-07 19:09:44 +0300

    10

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,94 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        System.out.println(" Задание 1");
+
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println(i);
         }
+
+        System.out.println(" Задание 2");
+
+        for (int a = 10; a >= 1; a--) {
+            System.out.println(a);
+        }
+
+        System.out.println(" Задание 3");
+
+        for (int b = 0; b < 17; b += 2) {
+            System.out.println("Четные числа " + b);
+        }
+        System.out.println(" Задание 4");
+
+        for (int c = 10; c >= -10; c--) {
+            System.out.println("Число " + c);
+        }
+
+        System.out.println(" Задание 4");
+
+        for (int d = 1904; d <= 2096; d += 4) {
+            System.out.println("Високосный год " + d);
+        }
+
+        System.out.println(" Задание 5");
+
+        for (int d = 1904; d <= 2096; d += 4) {
+            System.out.println("Високосный год " + d);
+        }
+
+        System.out.println(" Задание 6");
+
+        for (int f = 7; f <= 98; f += 7) {
+            System.out.println("Числа " + f);
+        }
+
+        System.out.println(" Задание 7");
+
+        for (int g = 1; g <= 512; g *= 2) {
+            System.out.println("Числа " + g);
+        }
+
+        System.out.println(" Задание 8");
+
+        int sum = 29000;
+        int total=0 ;
+        for (int p = 0; p <= 12; p++) {
+            total = total + sum;
+
+            System.out.println("Месяц " + p + " сумма накоплений равна " + total+ " рублей. ");
+        }
+
+        System.out.println(" Задание 9");
+
+        int sum1 = 29000;
+        int total1=0 ;
+        for (int r = 0; r <= 12; r++)
+        {
+            total1 = total1 + total1/100;
+            total1= total1 + sum1;
+
+            System.out.println("Месяц " + r + " сумма накоплений равна " + total1+ " рублей. ");
+        }
+
+        System.out.println(" Задание 10");
+
+
+int two=2;
+        for (int w = 1; w <= 10; w++) {
+
+            System.out.println( two +" * " + w+"= "+ two*w);
+
+
+        }
+
+
     }
 }
+
+
+
+
+
